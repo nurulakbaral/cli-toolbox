c13619599ddd930b303e7e3635a3d6365a82f117
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-07-14 19:51:14 +0700

    Chore(Project-Scaffold): Package.json adjustment

--- scripts/generates.mjs
+++ scripts/generates.mjs
@@ -3,7 +3,7 @@ import { spawnSync } from 'node:child_process'
 const dirs = ['./packages/project-scaffold/__tests__/examples/next-app-prettier-eslint']
 
 dirs.forEach((dir) => {
-  spawnSync('cd', [dir, '&&', 'pnpm', 'gen'], { stdio: 'inherit', shell: true })
+  spawnSync('cd', [dir, '&&', 'plop', '--plopfile', './plopfile.mjs'], { stdio: 'inherit', shell: true })
 })
 
 console.log('Successfully generated `examples` folders.')
