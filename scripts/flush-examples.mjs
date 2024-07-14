599f58bb71eaf71633aa2023d40d478ef43683f1
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-07-14 20:01:28 +0700

    Fix: Flush for package.json

--- scripts/flush-examples.mjs
+++ scripts/flush-examples.mjs
@@ -1,20 +1,36 @@
 import { globSync } from 'glob'
 import { rimrafSync } from 'rimraf'
 import * as path from 'node:path'
+import * as fs from 'node:fs'
 
-const dirs = ['./packages/project-scaffold/__tests__/examples/next-app-prettier-eslint/*']
+let dirs = ['./packages/project-scaffold/__tests__/examples/next-app-prettier-eslint/*']
 
-const excludedFiles = dirs
+let excludedFiles = dirs
   .map((dir) => [path.join(dir, '../package.json'), path.join(dir, '../plopfile.mjs')])
   .flat(Infinity)
 
-const files = globSync(dirs, {
+let files = globSync(dirs, {
   dot: true,
 })
 
 files.forEach((file) => {
   if (!excludedFiles.includes(file)) {
     rimrafSync(file)
+  }
+
+  if (file.includes('package.json')) {
+    try {
+      let content = fs.readFileSync(file, 'utf8')
+      let packageJson = JSON.parse(content)
+      let initialPackageJson = {
+        name: packageJson.name,
+        type: packageJson.type,
+      }
+      fs.writeFileSync(file, JSON.stringify(initialPackageJson, null, 2) + '\n', 'utf8')
+      console.log('Successfully flushed `package.json` file.')
+    } catch (err) {
+      console.error('Error:', err)
+    }
   }
 })
 
--- scripts/generates.mjs
+++ scripts/generates.mjs
@@ -1,6 +1,6 @@
 import { spawnSync } from 'node:child_process'
 
-const dirs = ['./packages/project-scaffold/__tests__/examples/next-app-prettier-eslint']
+let dirs = ['./packages/project-scaffold/__tests__/examples/next-app-prettier-eslint']
 
 dirs.forEach((dir) => {
   spawnSync('cd', [dir, '&&', 'plop', '--plopfile', './plopfile.mjs'], { stdio: 'inherit', shell: true })
