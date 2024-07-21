a5e1a443e64cc73a28dc9e364c6d25a32a648e67
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-07-21 16:13:23 +0700

    Fix(Project-Scaffold): Change the actions abstractions for next-app-structure

--- packages/project-scaffold/src/generators/next-app-structure/index.ts
+++ packages/project-scaffold/src/generators/next-app-structure/index.ts
@@ -1,10 +1,12 @@
-import { type NodePlopAPI } from 'plop'
+import { type NodePlopAPI, type ActionType } from 'plop'
 
-export interface TAnswers {
-  rootDir: 'y' | 'n'
-  publicDir: 'y' | 'n'
-  srcDir: string
+export type TAnswers = {
+  rootDir: boolean
+  publicDir: boolean
+  srcDir: boolean
 }
+export type TDynamicActionsFunction = (answers?: TAnswers) => ActionType[]
+export type TActions = TDynamicActionsFunction | ActionType[]
 
 export default function (plop: NodePlopAPI) {
   plop.setGenerator('next-app-structure', {
@@ -12,7 +14,8 @@ export default function (plop: NodePlopAPI) {
     prompts: [
       {
         name: 'rootDir',
-        message: 'Do you want to configure the root folder structure for the Next.js app (y/n)?',
+        type: 'confirm',
+        message: 'Do you want to configure the root folder structure for the Next.js app?',
         validate: (value) => {
           if (value !== 'y' && value !== 'n') {
             return 'Please enter "y" to continue or "n" to exit.'
@@ -23,7 +26,8 @@ export default function (plop: NodePlopAPI) {
       },
       {
         name: 'publicDir',
-        message: 'Do you want to configure the public folder (y/n)?',
+        type: 'confirm',
+        message: 'Do you want to configure the public folder?',
         validate: (value) => {
           if (value !== 'y' && value !== 'n') {
             return 'Please enter "y" to continue or "n" to exit.'
@@ -35,7 +39,7 @@ export default function (plop: NodePlopAPI) {
       {
         name: 'srcDir',
         type: 'confirm',
-        message: 'Do you want to configure the src folder (y/n)?',
+        message: 'Do you want to configure the src folder?',
         validate: (value) => {
           if (value !== 'y' && value !== 'n') {
             return 'Please enter "y" to continue or "n" to exit.'
@@ -45,37 +49,48 @@ export default function (plop: NodePlopAPI) {
         },
       },
     ],
-    actions: [
-      {
-        type: 'addMany',
-        skip: (answeres: TAnswers) => answeres.rootDir === 'n',
-        destination: './',
-        templateFiles: ['templates/@types/*', 'templates/ci/**/*', 'templates/archives/*', 'templates/scripts/*'],
-        globOptions: {
-          dot: true,
-        },
-        force: false,
-        abortOnFail: true,
-      },
-      {
-        type: 'addMany',
-        skip: (answeres: TAnswers) => answeres.publicDir === 'n',
-        destination: './',
-        templateFiles: ['templates/public/**/*'],
-        globOptions: {
-          dot: true,
-        },
-        force: false,
-        abortOnFail: true,
-      },
-      {
-        type: 'addMany',
-        destination: './',
-        templateFiles: ['templates/src/**/*'],
-        globOptions: {},
-        force: false,
-        abortOnFail: true,
-      },
-    ],
+    actions: function (ctx) {
+      const answers = ctx as TAnswers
+      const actions: TActions = []
+
+      if (answers.rootDir) {
+        actions.push({
+          type: 'addMany',
+          destination: './',
+          templateFiles: ['templates/@types/*', 'templates/ci/**/*', 'templates/archives/*', 'templates/scripts/*'],
+          globOptions: {
+            dot: true,
+          },
+          force: false,
+          abortOnFail: true,
+        })
+      }
+
+      if (answers.publicDir) {
+        actions.push({
+          type: 'addMany',
+          destination: './',
+          templateFiles: ['templates/public/**/*'],
+          globOptions: {
+            dot: true,
+          },
+          force: false,
+          abortOnFail: true,
+        })
+      }
+
+      if (answers.srcDir) {
+        actions.push({
+          type: 'addMany',
+          destination: './',
+          templateFiles: ['templates/src/**/*'],
+          globOptions: {},
+          force: false,
+          abortOnFail: true,
+        })
+      }
+
+      return actions
+    },
   })
 }
